314e6836b981a954ed595c8a83659d56e9da601d
Vova-fil <vfil.zp@gmail.com>
2022-04-25 02:41:12 +0300

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+//import { Contact } from './Contact/Contact';
+// import { Filter } from './Filter/Filter';
+// import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      // { id: 'id-1', name: 'Тарас Шевченко', number: '+3333333' },
+      // { id: 'id-2', name: 'Ліна костенко', number: '+6666666' },
+      // { id: 'id-3', name: 'Михайло Коцюбинський', number: '+4444444' },
+      // { id: 'id-4', name: 'Іван Франко', number: '+5555555' },
+    ],
+    // filter: '',
+    // name: '',
+  };
+
+  addContact = ({ contacts, id, name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => {
+      if (contacts.some(contact => contact.name === name)) {
+        return alert(`${contact.name} is already in contacts`);
+      }
+      return {
+        contacts: [contact, ...contacts],
+      };
+    });
+  };
+
+  render() {
+    // const { filter } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        {/* <Filter filter={filter} />
+         <ContactList /> */}
+      </div>
+    );
+  }
+}
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         textTransform: 'uppercase',
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+  };
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            value={name}
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <br />
+        <button type="submit" className="Form__button">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
